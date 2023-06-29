b70999ffc3ffcfe1cb4bb3b2a79d6d39766cdda2
pushpranjan2705 <90964355+pushpranjan2705@users.noreply.github.com>
2023-06-30 01:08:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,14 +8,18 @@ import authRoutes from './routes/authRoute.js';
 import categoryRoute from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import  cors from 'cors';
+import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-const _dirname = dirname(_filename);
+
+
+
 
 import {fileURLToPath} from 'url';
 dotenv.config();
 
 connectDB();
-const _filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app=express();
 
